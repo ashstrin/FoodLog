16cbda58618ad3775d483a399e20f47a2a4a9b15
ashleystringer <ashstrin@umail.iu.edu>
2018-08-29 13:56:20 -0400

    Adding some pages (?)

---
+++ mysql.js
@@ -0,0 +1,16 @@
+var mysql = require('mysql');
+
+console.log("Testing?");
+
+var con = mysql.createConnection({
+	host: "localhost",
+	user: "root",
+	password: "root",
+	database: "band_db",
+	port: 8889
+});
+
+con.connect(function(err){
+	if(err) throw err;
+	console.log("Connected");
+});
